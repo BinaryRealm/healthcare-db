227bf0cfdbfc0db59ddb6a55f437a2310c285d43
BinaryRealm <48455051+BinaryRealm@users.noreply.github.com>
2022-04-15 23:23:36 -0400

    added two functional requirements to queries

--- queries.sql
+++ queries.sql
@@ -46,3 +46,41 @@ WITH RECURSIVE search_graph(icd_code, parent_code, name, depth) AS (
     WHERE sg.icd_code = g.parent_code 
 )
 SELECT * FROM search_graph order by depth;
+
+
+/*Get all appointments that a patient has had at this practice.*/
+SELECT *
+FROM appointments a, patients p
+WHERE  a.patient_id = p.patient_id AND p.name = "John Smith"
+
+/*Prescribe a medication to a patient*/
+
+/*Provide a patient a referral to a specialized doctor*/
+
+/*Create a new patient, and add their family history*/
+
+/*Get all patients based on their insurance provider*/
+
+SELECT p.patient_id, p.name
+FROM patients p, insurance_covers c, insurance_providers i
+WHERE p.patient_id = c.patient_id 
+    AND c.provider_id = i.provider_id 
+    AND i.insurance_name = "Unitedhealth Group"
+
+/*Get all patients that had appointment with certain doctor in the last 7 days
+ Used for COVID-19 tracing to notify patients*/
+
+/*Get all info about a patient in a single view.
+(doctor wants to get all info on patient)*/
+
+/*Get all patients who are vaccinated for a specific vaccine.*/
+
+/*Get all patients who were prescribed a specific drug.
+In case of recall, or price jumps of brand-name */
+
+/*Get contact info of a patient’s emergency contact*/
+
+/*See the date of the most recent appointment of a patient
+For front-desk appointment scheduling.*/
+
+/*Get health metrics (average, min, max) of patient history within a specified time range.*/
