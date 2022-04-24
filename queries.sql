1053ac294635f60edb86e69d6b2421ddc3d8b89b
Nathan Larkin <nathanlarkin.usa@gmail.com>
2022-04-23 21:38:13 -0400

    improved alias names of recursive queries

--- queries.sql
+++ queries.sql
@@ -1,25 +1,25 @@
 /* HOW TO FIND ALL PARENT (SUB)CATEGORIES A CONDITION 'R94118' IS A PART OF */
 WITH RECURSIVE search_graph(icd_code, parent_code, name, depth) AS (
-    SELECT g.icd_code, g.parent_code, g.name, 1
-    FROM medical_conditions g
-    where g.icd_code ='R94118'
+    SELECT mc.icd_code, mc.parent_code, mc.name, 1
+    FROM medical_conditions mc
+    where mc.icd_code ='R94118'
   UNION ALL
-    SELECT g.icd_code, g.parent_code, g.name, sg.depth + 1
-    FROM medical_conditions g, search_graph sg
-    WHERE g.icd_code  = sg.parent_code
+    SELECT mc.icd_code, mc.parent_code, mc.name, sg.depth + 1
+    FROM medical_conditions mc, search_graph sg
+    WHERE mc.icd_code = sg.parent_code
 )
 SELECT * FROM search_graph order by depth;
 
 
 /* HOW TO FIND ALL CHILDREN SUB(CATEGORIES)/CODES THE CURRENT (SUB)CATEGORY 'R93' HAS */
 WITH RECURSIVE search_graph(icd_code, parent_code, name, depth) AS (
-    SELECT g.icd_code, g.parent_code, g.name, 1
-    FROM medical_conditions g
-    where g.icd_code = 'R93'
+    SELECT mc.icd_code, mc.parent_code, mc.name, 1
+    FROM medical_conditions mc
+    where mc.icd_code = 'R93'
   UNION ALL
-    SELECT g.icd_code, g.parent_code, g.name, sg.depth + 1
-    FROM medical_conditions g, search_graph sg
-    WHERE sg.icd_code = g.parent_code 
+    SELECT mc.icd_code, mc.parent_code, mc.name, sg.depth + 1
+    FROM medical_conditions mc, search_graph sg
+    WHERE sg.icd_code = mc.parent_code 
 )
 SELECT * FROM search_graph order by depth;
 
